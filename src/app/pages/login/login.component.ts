44809cbd78b78c373a441f515137127544e916c0
siacerola <si.acerola@gmail.com>
2023-06-22 09:46:10 +0700

    import module FormGroup, control, validator

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  showText = false
+
+  loginForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required,Validators.minLength(8)])
+
+  })
+
+  buttonSubmit() {
+    console.log(this.showText);
+    console.log(this.loginForm.valid);
+
+    console.log(this.loginForm.value);
+  }
 
 }
