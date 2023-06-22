c17caf2a428c421f7bf2dbacee3ecbbb51029777
siacerola <si.acerola@gmail.com>
2023-06-22 09:55:19 +0700

    move user form to user list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { UserModel } from './model/user.model';
 
 @Component({
   selector: 'app-root',
@@ -9,80 +7,11 @@ import { UserModel } from './model/user.model';
 })
 export class AppComponent {
   title = 'angularApp';
-  showText = false
 
-  loginForm = new FormGroup({
-    username: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required,Validators.minLength(8)])
-
-  })
-
-  userForm = new FormGroup({
-    id:new FormControl('',[Validators.required]),
-    name:new FormControl('',[Validators.required]),
-    address: new FormControl('', [Validators.required]),
-    phoneNumber:new FormControl('',[Validators.required]),
-    email:new FormControl('',[Validators.required])
-  })
-
-
-  buttonOnClick() {
-    console.log("tombol di klik");
-    this.showText = !this.showText
-    console.log(this.showText);
-  }
-
-  buttonSubmit() {
-    console.log(this.showText);
-    console.log(this.loginForm.valid);
-
-    console.log(this.loginForm.value);
-  }
-
-  userList:UserModel[]=[]
-
-  buttonSubmitForm() {
-    let doc = new UserModel({
-      id: this.userForm.value.id,
-      name:this.userForm.value.name,
-      address:this.userForm.value.address,
-      phoneNumber:this.userForm.value.phoneNumber,
-      email:this.userForm.value.email
-    })
-
-    this.userList.push(doc)
-
-  }
-
-  // userBudi: UserModel = new UserModel({
-  //   id: 1,
-  //   name: "budi",
-  //   address: "alamat budi",
-  //   phoneNumber: "nomor WA budi",
-  //   email:"email budi"
-  // })
-
-  // userAji: UserModel = new UserModel({
-  //   id: 2,
-  //   name: "Aji",
-  //   address: "alamat Aji",
-  //   phoneNumber: "nomor WA Aji",
-  //   email:"email Aji"
-  // })
-
-  // userAsep: UserModel = new UserModel({
-  //   id: 3,
-  //   name: "Asep",
-  //   address: "alamat Asep",
-  //   phoneNumber: "nomor WA Asep",
-  //   email:"email Asep"
-  // })
-
-  // userList: UserModel[] = [
-  //   this.userBudi,
-  //   this.userAji,
-  //   this.userAsep
-  // ]
-
+  // buttonOnClick() {
+  //   console.log("tombol di klik");
+  //   this.showText = !this.showText
+  //   console.log(this.showText);
+  // }
 }
 
--- src/app/pages/user-list/user-list.component.ts
+++ src/app/pages/user-list/user-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserModel } from '../../model/user.model';
 
 @Component({
   selector: 'app-user-list',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent {
+  userForm = new FormGroup({
+    id:new FormControl('',[Validators.required]),
+    name:new FormControl('',[Validators.required]),
+    address: new FormControl('', [Validators.required]),
+    phoneNumber:new FormControl('',[Validators.required]),
+    email:new FormControl('',[Validators.required])
+  })
 
+  userList:UserModel[]=[]
+
+  buttonSubmitForm() {
+    let doc = new UserModel({
+      id: this.userForm.value.id,
+      name:this.userForm.value.name,
+      address:this.userForm.value.address,
+      phoneNumber:this.userForm.value.phoneNumber,
+      email:this.userForm.value.email
+    })
+
+    this.userList.push(doc)
+
+  }
 }
